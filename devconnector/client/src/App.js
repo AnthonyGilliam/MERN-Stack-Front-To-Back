78199184fe100d9ad4f5e0ebef557c0f255bcf88
Anthony <anthony.gilliam@gmail.com>
2023-04-07 03:53:48 -0400

    7:37 Section 7: Redux Setup & Alerts - Creating a Redux Store

--- devconnector/client/src/App.js
+++ devconnector/client/src/App.js
@@ -1,22 +1,29 @@
 import React from 'react';
-import  {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+// Redux
+import {Provider} from 'react-redux';
+import store from './store';
+
 import './App.css';
 
 const App = () => (
-    <Router>
-        <Navbar />
-        <Route exact path="/" component={Landing} />
+
+    <Provider store={store}>
+        <Router>
+            <Navbar/>
+            <Route exact path="/" component={Landing}/>
             <main className="container">
                 <Switch>
-                    <Route exact path="/register" component={Register} />
-                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/register" component={Register}/>
+                    <Route exact path="/login" component={Login}/>
                 </Switch>
             </main>
-    </Router>
+        </Router>
+    </Provider>
 );
 
 export default App;
